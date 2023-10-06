f3acdb137d08b6e38d3100caf4c0a8e3ca084643
frede <frederik@rovi.dk>
2023-10-06 17:48:30 +0200

    upload now works

--- src/routes/azure-storage-routes-test.ts
+++ src/routes/azure-storage-routes-test.ts
@@ -2,8 +2,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import {
   GetSingleImage,
   GetSinglePodcast,
-  UploadImageSasToken,
-  UploadPodcastSasToken,
+  UploadSASToken,
 } from "../utils/azure-storage";
 
 async function azureStorageRoutes(server: FastifyInstance) {
@@ -54,13 +53,13 @@ async function azureStorageRoutes(server: FastifyInstance) {
   );
 
   server.post(
-    "/getuploadimagesastoken",
+    "/getuploadsastoken",
     {
       schema: {
         body: {
           type: "object",
           properties: {
-            filename: { type: "string" },
+            containername: { type: "string" },
           },
         },
       },
@@ -68,30 +67,7 @@ async function azureStorageRoutes(server: FastifyInstance) {
     async (request: FastifyRequest, reply: FastifyReply) => {
       const body: any = request.body;
       try {
-        const sasToken = await UploadImageSasToken(body.filename);
-        return reply.code(200).send(sasToken);
-      } catch (err: any) {
-        return reply.code(404).send(err.message);
-      }
-    }
-  );
-
-  server.post(
-    "/getuploadpodcastsastoken",
-    {
-      schema: {
-        body: {
-          type: "object",
-          properties: {
-            filename: { type: "string" },
-          },
-        },
-      },
-    },
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      const body: any = request.body;
-      try {
-        const sasToken = await UploadPodcastSasToken(body.filename);
+        const sasToken = await UploadSASToken(body.containername);
         return reply.code(200).send(sasToken);
       } catch (err: any) {
         return reply.code(404).send(err.message);
--- src/utils/azure-storage-helpers.ts
+++ src/utils/azure-storage-helpers.ts
@@ -6,18 +6,16 @@ import {
 import { DefaultAzureCredential } from "@azure/identity";
 
 const TWENTYFOUR_HOURS: any = 24 * 60 * 60 * 1000;
-const PRODUCTION: boolean = process.env.NODE_ENV === "production";
-const AZURITEACCOUNTKEY: any = process.env.AZURITEACCOUNTKEY;
 
-// Create a service SAS for a blob
-// This function only works in the NODE.JS runtime
-export async function createSASToken(
+export async function createSASTokenREAD(
   accountName: string,
   blobClient: any,
   containerClient: any,
   blobName: string,
   auth: any
 ) {
+  const PRODUCTION = process.env.NODE_ENV === "production";
+
   const sasOptions: any = {
     containerName: containerClient.containerName,
     blobName: blobName,
@@ -41,13 +39,49 @@ export async function createSASToken(
   return sasToken;
 }
 
+export async function createSASTokenREADWRITE(
+  accountName: string,
+  blobClient: any,
+  containerClient: any,
+  blobName: string,
+  auth: any
+) {
+  const PRODUCTION = process.env.NODE_ENV === "production";
+
+  const sasOptions: any = {
+    containerName: containerClient.containerName,
+    blobName: blobName,
+    startsOn: new Date(new Date().valueOf() - TWENTYFOUR_HOURS),
+    expiresOn: new Date(new Date().valueOf() + TWENTYFOUR_HOURS),
+    permissions: BlobSASPermissions.parse("rw"),
+  };
+
+  const userDelegationKey = PRODUCTION
+    ? await blobClient.getUserDelegationKey(
+      new Date(new Date().valueOf() - TWENTYFOUR_HOURS),
+      new Date(new Date().valueOf() + TWENTYFOUR_HOURS)
+    )
+    : null;
+
+  const sasToken = generateBlobSASQueryParameters(
+    sasOptions,
+    PRODUCTION ? userDelegationKey : auth,
+    accountName
+  ).toString();
+  return sasToken;
+}
+
+
+
 export async function azureAuth(accountName: string) {
+  const PRODUCTION = process.env.NODE_ENV === "production";
   let auth = null;
   if (PRODUCTION) {
     // Use managed identity in production for our static web app
     auth = new DefaultAzureCredential();
   } else {
     // Use Azurite account key in development
+    const AZURITEACCOUNTKEY: any = process.env.AZURITEACCOUNTKEY;
     auth = new StorageSharedKeyCredential(accountName, AZURITEACCOUNTKEY);
   }
   return auth;
