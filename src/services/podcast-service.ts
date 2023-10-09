17889facae77d2673ea615b60457711d2efe1bec
Frederik Vilhelmsen <frederik@rovi.dk>
2023-10-09 15:38:39 +0200

    Azure storage for podcasts

--- src/services/podcast-service.ts
+++ src/services/podcast-service.ts
@@ -1,6 +1,5 @@
 import prisma from "../utils/orm-connection";
-import { PodcastCreateInput, ReadSinglePodcastSchema, deletePodcastSchema } from "../schemas/podcast-schemas";
-import { GetSingleImage } from "../utils/azure-storage";
+import { PodcastCreateInput, podcastSchema, deletePodcastSchema } from "../schemas/podcast-schemas";
 
 export async function createPodcast(input: PodcastCreateInput) {
     return await prisma.podcast.create({
@@ -15,27 +14,48 @@ export async function createPodcast(input: PodcastCreateInput) {
 }
 
 
-export async function findSinglePodcast(input: ReadSinglePodcastSchema) {
-    let podcast = await prisma.podcast.findUnique({
-        where: {
-            id: input.id
-        },
-
-    })
-
-
-}
-
-export async function getAllUsersFollowPodcasts(input: ReadSinglePodcastSchema) {
+export async function findSinglePodcast(input: podcastSchema) {
     return await prisma.podcast.findUnique({
         where: {
             id: input.id
         },
+        // include: {
+        //     fk_user_id: true,
+        //     Podcast_liked_by_user: true,
+        //     Podcast_viewed_by_user: true,
+        //     Comment: {
+        //         include: {
+        //             fk_user_id: true,
+        //             Comments_liked: true
+        //         }
+        //     }
+        // }
+    });
+};
+
+export async function getAllUsersFollowPodcasts(input: podcastSchema) {
+    const followedUsers = await prisma.user.findMany({
+        where: {
+            followedBy: {
+                some: {
+                    id: Number(input.id)
+                }
+            }
+        }
+    })
+
+    const followedUsersId = followedUsers.map((user) => {
+        return user.id
+    })
+
+    return await prisma.podcast.findMany({
+        where: {
+            userId: {
+                in: followedUsersId
+            }
+        }
     })
 }
-
-
-
 
 
 export async function updatePodcast(input: PodcastCreateInput) {
